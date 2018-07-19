05e158a38ba26a4039fa39fe82bb288a2466e6db
Seerchand <chandanis562@gmail.com>
2018-07-19 11:42:31 +0530

    dinu

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ getfile("d.json",function(text){
   console.log(data);
   details(data.basics);
   career(data.careerobjectives);
+  educate(data.education);
+  skill(data.technicalqualifications);
+  achievement(data.achievements);
 })
 var child=document.querySelector(".child1");
 function details(det){
@@ -46,10 +49,62 @@ function details(det){
 }
 var childs=document.querySelector(".child2");
 function career(careerinfo){
-  var text=document.createElement("h1");
-  text.textContent="CAREER OBJECTIVES:"
+  var text=document.createElement("h2");
+  text.textContent=("CAREER OBJECTIVES:");
   childs.appendChild(text);
+  var hr=document.createElement("hr");
+  childs.appendChild(hr);
+
   var obj=document.createElement("h2");
   obj.textContent = careerinfo.info;
   childs.appendChild(obj);
+}
+
+function educate(edu){
+
+  var h=document.createElement("h2");
+  h.textContent = ("EDUCATIONAL QUALIFICATION");
+  childs.appendChild(h);
+
+  var hr=document.createElement("hr");
+  childs.appendChild(hr);
+
+  for(i=0;i<edu.length;i++){
+  var deg=document.createElement("h3");
+  deg.textContent = edu[i].degree;
+  childs.appendChild(deg);
+
+  var edul=document.createElement("ul");
+  var eduli=document.createElement("li");
+  eduli.textContent = edu[i].institute;
+  edul.appendChild(eduli);
+  childs.appendChild(edul);
+
+
+  var edul=document.createElement("ul");
+  var eduli=document.createElement("li");
+  eduli.textContent = edu[i].year;
+  edul.appendChild(eduli);
+  childs.appendChild(edul);
+}
+}
+
+function skill(skl){
+
+  var s = document.createElement("h2");
+  s.textContent = ("TECHNICAL QUALIFICATIONS");
+  childs.appendChild(s);
+
+  var hr = document.createElement("hr");
+  childs.appendChild(hr);
+
+  var skilldata=document.createElement("table");
+  skilldata.border="2";
+  childs.appendChild(skilldata);
+
+  tabledata="";
+  for(i=0;i<skl.length;i++){
+    tabledata+="<tr><td>"+skl[i].tittle+"</td><td>"+skl[i].data+"</td></tr>";
+  }
+  skilldata.innerHTML=tabledata;
 }
